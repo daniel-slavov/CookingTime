a58bd39af4fd016472ac0b7114ee80a11de4c7c2
daniel-slavov <daniel.slavov@aol.com>
2017-09-20 02:11:33 +0300

    Created project

---
+++ CookingTime/CookingTime/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CookingTime.Startup))]
+namespace CookingTime
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
