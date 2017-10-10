e6ecd09ca12112a69eeba2243bf7736ed53f4862
daniel-slavov <daniel.slavov@aol.com>
2017-10-10 11:58:10 +0300

    added test project

---
+++ CookingTime/CookingTime.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CookingTime.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CookingTime.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8685365b-c93c-4709-97df-19ff68f0299c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CookingTime/CookingTime.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace CookingTime.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
